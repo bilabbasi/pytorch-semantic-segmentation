bb538406a0860791adc14fd0baee23dc43d06eb2
Bilal Abbasi <bilal.abbasi.ba@gmail.com>
2017-11-25 09:26:34 -0500

    minor

--- cc-runner.py
+++ cc-runner.py
@@ -20,7 +20,7 @@ cpus = 16
 log_dir = os.path.join('..','logs')
 s.write('#!/bin/bash\n')
 s.write('#SBATCH --account=def-oberman\n')
-s.write('#SBATCH --time='+time.strftime('%H:%M:%S',t)+' \t\t# max time (HH:MM:SS)\n')
+# s.write('#SBATCH --time='+time.strftime('%H:%M:%S',t)+' \t\t# max time (HH:MM:SS)\n')
 s.write('#SBATCH --job-name='+'test\n')
 s.write('#SBATCH --mem='+str(mem)+'M \t\t\t# memory per node\n')
 if not args.no_cuda:
