5318cc239f647de88d1ec8d8ca3dbcc1213a7abb
Bilal Abbasi <bilal.abbasi.ba@gmail.com>
2017-12-07 13:36:08 -0500

    minor

--- cc-runner.py
+++ cc-runner.py
@@ -1,4 +1,4 @@
-# # !/usr/bin/env python3
+# !/usr/bin/env python3
 # This script generates a batch script to be run on the Compute Canada server
 import argparse
 import subprocess
@@ -18,20 +18,31 @@ gpus = 4
 mem = 16000
 cpus = 16
 log_dir = os.path.join('..','logs')
-s.write('#!/bin/bash\n')
-s.write('#SBATCH --account=def-oberman\n')
-# s.write('#SBATCH --time='+time.strftime('%H:%M:%S',t)+' \t\t# max time (HH:MM:SS)\n')
-s.write('#SBATCH --job-name='+'test\n')
-s.write('#SBATCH --mem='+str(mem)+'M \t\t\t# memory per node\n')
-# if not args.no_cuda:
-#     s.write('#SBATCH --gres=gpu:'+str(gpus)+' \t\t# request 4 gpus per node\n')
-s.write('#SBATCH --cpus-per-task='+str(cpus) +'\n')
+
+# For Python 2
+# s.write('#!/bin/bash\n')
+# s.write('#SBATCH --account=def-oberman\n')
+# # s.write('#SBATCH --time='+time.strftime('%H:%M:%S',t)+' \t\t# max time (HH:MM:SS)\n')
+# s.write('#SBATCH --job-name='+'test\n')
+# s.write('#SBATCH --mem='+str(mem)+'M \t\t\t# memory per node\n')
+# s.write('#SBATCH --cpus-per-task='+str(cpus) +'\n')
+# # s.write('#SBATCH --output='+log_dir+'/log.out\n')
+# s.write('#SBATCH --signal=15@30 \t\t#Send SIGTERM 30 seconds before time out\n')
+# s.write('\n\nsource ~/anaconda3/bin/activate\n')
+# s.write('python -u /home/babbasi/level-set/pytorch-image-segmentation/train/voc-fcn/train-test.py')
+# s.close()
+
+# For Python 3
+print('#!/bin/bash\n',file=s)
+print('#SBATCH --account=def-oberman\n',file=s)
+# s.write('#SBATCH --time='+time.strftime('%H:%M:%S',t)+' \t\t# max time (HH:MM:SS)\n',file=s)
+print('#SBATCH --job-name='+'test\n',file=s)
+print('#SBATCH --mem='+str(mem)+'M \t\t\t# memory per node\n',file=s)
+print('#SBATCH --cpus-per-task='+str(cpus) +'\n',file=s)
 # s.write('#SBATCH --output='+log_dir+'/log.out\n')
-# s.write('#SBATCH --output='+'~/level-set/pytorch-image-segmentation/train/voc-fcn/train-test.py' + '\n')
-s.write('#SBATCH --signal=15@30 \t\t#Send SIGTERM 30 seconds before time out\n')
-s.write('\n\nsource ~/anaconda3/bin/activate\n')
-s.write('python -u /home/babbasi/level-set/pytorch-image-segmentation/train/voc-fcn/train-test.py')
-# print('python -u ~/level-set/pytorch-image-segmentation/train/voc-fcn/train.py' ,file=ccscript, flush=True)
+print('#SBATCH --signal=15@30 \t\t#Send SIGTERM 30 seconds before time out\n',file=s)
+print('\n\nsource ~/anaconda3/bin/activate\n',file=s)
+print('python -u /home/babbasi/level-set/pytorch-image-segmentation/train/voc-fcn/train-test.py',file=s,flush=True)
 # print('python -u ~/optimization/bgd/main.py  --data ~/scratch'+ '\\\n'
 #         '\t--momentum ' + str(args.momentum) + '\\\n'
 #         '\t--lipshitz ' + str(args.lipshitz)+'\\\n'+
@@ -50,6 +61,5 @@ s.write('python -u /home/babbasi/level-set/pytorch-image-segmentation/train/voc-
 #         no_cuda_str+
 #         '\t--model '+args.model
 #     ,file=ccscript, flush=True)
-s.close()
 
 subprocess.call(['sbatch' ,'cc-runner.sh'])
