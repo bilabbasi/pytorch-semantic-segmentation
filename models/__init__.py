7d6059e6c8cb833fa5077c8a6bf9035984b291f9
Bilal Abbasi <bilal.abbasi.ba@gmail.com>
2017-12-12 10:49:53 -0500

    minor

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.insert(0, '/home/babbasi/level-sets/pytorch-semantic-segmentation/models') # compute canada root
+sys.path.insert(0, '/home/babbasi/level-sets/pytorch-semantic-segmentation') # compute canada root
 
 from .duc_hdc import *
 from .fcn16s import *
